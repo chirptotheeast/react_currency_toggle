19d7245bdbdd3540fbde074b635f48c1e9787d04
chirptotheeast <alyssa.easterly@yahoo.com>
2021-07-17 20:23:51 -0400

    conversion functionality added in

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
   const [fromCurrency, setFromCurrency] = useState([])
   const [toCurrency, setToCurrency] = useState([])
+  const [exchangeRate, setExchangeRate] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+
+  let toAmount, fromAmount
+  if (amountInFromCurrency){
+    fromAmount = amount
+    toAmount = amount + exchangeRate
+  } else {
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
+
 
   //currencyOptions represents current state of app, 2nd arg allows us 
   // set the currencyOptions to something new
@@ -24,6 +37,7 @@ function App() {
         setCurrencyOptions([data.base, ...Object.keys(data.rates)])
         setFromCurrency(data.base)
         setToCurrency(firstCurrency)
+        setExchangeRate(data.rates[firstCurrency])
       })
   }, [])
 
@@ -35,6 +49,16 @@ function App() {
 
   //fetch BASE_URL and conver it to JSON 
 
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  } 
+
+  function handleToAmountChange(e){
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
+
   return (
     <>
       <h1> Convert</h1>
@@ -42,12 +66,16 @@ function App() {
         currencyOptions={currencyOptions}
         selectedCurrency={fromCurrency}
         onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
       />
       <div className="equals">=</div>
       <CurrencyRow
         currencyOptions={currencyOptions}
         selectedCurrency={toCurrency}
         onChangeCurrency={(e) => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
       />
     </>
   );
